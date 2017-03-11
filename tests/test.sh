251c7bbd09b7467f89ff1758dcc9f3fd34bb568b
Vladislav Vayngart <vvayngart@yahoo.com>
2017-03-11 11:47:58 -0600

    continued to implement

--- tests/test.sh
+++ tests/test.sh
@@ -1,7 +1,7 @@
 echo "+++++++++++++++++++++++++++++++++ RUNNING regression test1"
-../$1/test1.exe actions2.txt actionsResultsExpected2.txt
+../$1/SimpleCross.exe actions2.txt actionsResultsExpected2.txt
 echo "+++++++++++++++++++++++++++++++++ done RUNNING regression test1"
 echo ""
 echo "+++++++++++++++++++++++++++++++++ RUNNING regression test2"
-../$1/test1.exe actions3.txt actionsResultsExpected3.txt
+../$1/SimpleCross.exe actions3.txt actionsResultsExpected3.txt
 echo "+++++++++++++++++++++++++++++++++ done RUNNING regression test2"
